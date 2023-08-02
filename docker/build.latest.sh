9e893698d08a1607adc86feaee3863c8ad9b20c8
kimsuk <kimsuk@linginfo.com>
2023-08-02 13:51:31 +0800

    docker.

--- docker/build.latest.sh
+++ docker/build.latest.sh
@@ -3,6 +3,6 @@
 unset KUBECONFIG
 
 cd .. && docker build -f docker/Dockerfile.latest \
-             -t zhayujie/chatgpt-on-wechat .
+             -t kimsuk/chatgpt-on-wechat .
 
-docker tag zhayujie/chatgpt-on-wechat zhayujie/chatgpt-on-wechat:$(date +%y%m%d)
+docker tag kimsuk/chatgpt-on-wechat kimsuk/chatgpt-on-wechat:$(date +%y%m%d)
